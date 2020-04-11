b179665f16cc4d69063ee6888c3f128e2492f981
Sang Soo Ra <nadang82@gmail.com>
2020-04-11 12:49:21 +0200

    finished first app

---
+++ src/components/app.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [{ id: 'QYkX9IMHthYn0Y3pcG' },
+        { id: '3otPovknsXChnFfbqg' }],
+      selectedGifId: 'QYkX9IMHthYn0Y3pcG'
+    };
+
+    this.search('avengers');
+  }
+
+  search = (query) => {
+    giphy('pPdrsNmTTIrlNex09UiRgdBltrsaPnGO').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (err, res) => {
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar search={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
